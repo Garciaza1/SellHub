ad3035bc99d21b37cb271c5f4e33ec349b8f74f9
Gustavo Ribeiro Garcia <102680004+Garciaza1@users.noreply.github.com>
2024-06-04 11:04:56 -0300

    controller e rota de Compras

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -22,55 +22,56 @@ const userController = {
 
     createUser: (req, res) => {
 
-        const { nome, email, senha, tipo } = req.body;
+        const { nome, email, senha, tipo, tel, endereco, cpf, cep } = req.body;
+        const query = 'INSERT INTO usuario (nome, email, senha, tipo, tel, endereco, cpf, cep) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
 
-        const query = 'INSERT INTO usuario (nome, email, senha, tipo) VALUES (?, ?, ?, ?)';
-
-        db.query(query, [nome, email, senha, tipo], (err, results) => {
-
+        db.query(query, [nome, email, senha, tipo, tel, endereco, cpf, cep], (err, results) => {
             if (err) {
-                res.status(500).json({ error: 'Failed to create user' });
+                res.status(500).json({ error: 'Failed to create user, may already exist. ' + err });
                 return;
             }
 
-            res.status(201).json({ id: results.insertId, name, email });
-
+            console.log('Usuario cadastrado do tipo: ' + tipo);
+            res.status(201).json({ id: results.insertId, nome, email, senha, tipo, tel, endereco, cpf, cep });
         });
 
     },
 
     loginUser: (req, res) => {
+
+        // lembrar de dar console log pra ver como e oque esta vindo do body
         const { email, senha } = req.body;
         const query = 'SELECT * FROM usuario WHERE email = ?';
-        
+
         db.query(query, [email], (err, results) => {
             if (err) {
                 return res.status(500).json({ error: 'Failed to get user' });
             }
-            
+
             if (results.length === 0) {
                 return res.status(401).json({ error: 'User not found' });
             }
-            
+
             const user = results[0];
+
             if (user.senha !== senha) {
                 return res.status(401).json({ error: 'Invalid password' });
             }
-            
+
             console.log('User logged in');
-            console.log('User: ' + user);
-            
-            res.status(200).json({ message: 'Validated!' });
+            // console.log('User: ' + JSON.stringify(user));
+
+            res.status(200).json({ message: 'Validated!', user: user });
         });
     },
-    
+
     getUser: (req, res) => {
-        
+
         const { id } = req.params;
 
         const query = 'SELECT * FROM usuario WHERE id = ?';
 
-        db.query(query, [id],(err, results) => {
+        db.query(query, [id], (err, results) => {
 
             if (err) {
                 res.status(500).json({ error: 'Failed to fetch users' });
@@ -78,6 +79,22 @@ const userController = {
             }
 
             res.json(results);
+
+        });
+    },
+    fetchUser: (req, res) => {
+
+        const { email } = req.params;
+
+        const query = 'SELECT * FROM usuario WHERE email = ?';
+
+        db.query(query, [email], (err, results) => {
+
+            if (err) {
+                res.status(500).json({ error: 'Failed to fetch users' });
+                return;
+            }
+            res.json(results[0]);
 
         });
 
