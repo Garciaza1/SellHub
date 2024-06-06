87817960669e4c3c4830240de474cbc36fb2c997
Gustavo Ribeiro Garcia <102680004+Garciaza1@users.noreply.github.com>
2024-06-06 01:05:46 -0300

    protected routes and etc

--- Backend/index.js
+++ Backend/index.js
@@ -11,8 +11,8 @@ const port = process.env.PORT || 5000;
 // app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors());
-app.use(express.json()); 
-
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: false}));
 app.use('/', router);  
 
 app.listen(port, () => {
